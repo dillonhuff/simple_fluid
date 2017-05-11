87677465f7710e8b4c8882fe649d49ce6a9b176a
Dillon Huff <dillonhuff@gmail.com>
2017-05-11 03:06:41 -0700

    Segfault in very simple 2d array code?

--- src/scalar_field.h
+++ src/scalar_field.h
@@ -3,8 +3,35 @@
 namespace sfluid {
 
   class scalar_field {
+  protected:
+    int x_len, y_len;
+    double* values;
+
   public:
-    scalar_field(const int x_elems, const int y_elems) {}
+    scalar_field(const int x_elems, const int y_elems) :
+      x_len(x_elems), y_len(y_elems) {
+
+      values = new double(x_len*y_elems);
+
+      for (int i = 0; i < x_len; i++) {
+	for (int j = 0; j < y_len; j++) {
+	  values[i*y_len + j] = 0.0;
+	}
+      }
+    }
+
+    void set(const int i, const int j, const double value) {
+      values[i*y_len + j] = value;
+    }
+
+    double get(const int i, const int j) const {
+      return values[i*y_len + j];
+    }
+
+    ~scalar_field() {
+      delete[] values;
+    }
+
   };
 
 }
--- test/grid_tests.cpp
+++ test/grid_tests.cpp
@@ -9,7 +9,10 @@ namespace sfluid {
   TEST_CASE("Set grid cell") {
     scalar_field field(200, 200);
 
-    
+    field.set(0, 0, 12.3);
+
+    REQUIRE(field.get(0, 0) == 12.3);
+    REQUIRE(field.get(0, 1) == 0.0);
   }
   
 }
